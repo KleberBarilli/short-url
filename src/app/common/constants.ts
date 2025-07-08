ebff551dd52117c15abb0d72e114162f7ad753a7
kleberbarilli <kleber.barilli73@gmail.com>
2025-07-08 20:23:11 -0300

    feat: 🎸 create jwt strategy & controller guard

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -1,3 +1,5 @@
 export const SHORT_CODE_LENGTH = 6;
 export const CREATE_SHORT_CODE_MAX_RETRIES = 3;
 export const DEFAULT_HASH_SALT = 12;
+export const API_SECRET = process.env.API_SECRET || 'default_secret';
+export const DEFAULT_TOKEN_EXPIRATION = '7d';
---
+++ src/app/core/user/auth/jwt-strategy.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { API_SECRET } from 'src/app/common/constants';
+import { TokenPayload } from './types/token-payload';
+
+@Injectable()
+export class JwtStrategyService extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: API_SECRET,
+    });
+  }
+
+  validate({ sub, name }: TokenPayload): { userId: string; name: string } {
+    return { userId: sub, name: name };
+  }
+}
